489e8dc3cf70e0ba65aa52c4f10f5362c2234b3b
Parimala <129543087+parimalaasam@users.noreply.github.com>
2024-11-09 21:57:06 +0530

    Create employee.sql

---
+++ employee.sql
@@ -0,0 +1,22 @@
+ CREATE TABLE Employee
+ (
+ "E_ID" varchar(20) primary key not null,
+"E_Name" varchar(30),
+ "E_Address" varchar(40),
+ "E_Contact" number(10),
+ "E_Salary" number(8),
+ "E_Timings" varchar(8),
+ "DO_Joining" varchar(12),
+ "Qualifications" varchar(30)
+ );
+
+INSERT into Employee VALUES(10253,'akhil','Jalandhar', 9693959676, 10000, '8
+hours','2/12/1990','BTECH');
+ INSERT into Employee VALUES(10252,'jojo','Phagwara', 9693959677, 20000, '10
+hours','12/11/1990','BTECH');
+ INSERT into Employee VALUES(10258,'stella','Jagtial', 9693959678, 15000, '8
+hours','2/12/1990','BARC');
+ INSERT into Employee VALUES(10251,'monika','Karimnagar', 9693959679, 15000, '8
+hours','3/11/1990','BTECH');
+ INSERT into Employee VALUES(11259,'himanshi','Warangal', 9693959680, 10000, '10
+hours','2/10/1990','Degree'); 
